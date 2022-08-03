b2b0501011a7567b63a4437656dc90c401522464
datbung <bungdat@gmail.com>
2022-08-03 10:45:48 +0700

    update

--- src/pages/tabBar/myhome/index.ts
+++ src/pages/tabBar/myhome/index.ts
@@ -30,8 +30,6 @@ Page({
     var parts = this.round(number + fee, 0).toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands);
     return parts.join(thousands);
-}
-
-
+},
 });
 
