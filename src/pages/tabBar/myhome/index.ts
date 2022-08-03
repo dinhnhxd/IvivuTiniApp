09495115b21de4c70e523b86049298c8d5088b19
datbung <bungdat@gmail.com>
2022-08-03 10:45:06 +0700

    update

--- src/pages/tabBar/myhome/index.ts
+++ src/pages/tabBar/myhome/index.ts
@@ -1,18 +1,37 @@
+
 Page({
   data: {
-    fixedHeader: false,
-    items: Array.from(Array(100).keys()),
-    iconSize: [20, 30, 40, 50, 60],
-    iconColor: ['red', 'yellow', 'blue', 'green'],
-    iconType: ['adjustment']
   },
-  onPageScroll(event: { scrollTop: number; }) {
-    this.setData({ fixedHeader: event.scrollTop > 20 });
+  onLoad(){
+      this.setData({ loading: true });
+      my.request({
+        url: 'https://beta-olivia.ivivu.com/mobile/OliviaApis/TopDeals?pageIndex=1&pageSize=200',
+        method: 'POST',
+        headers:
+        {
+          apisecret: '2Vg_RTAccmT1mb1NaiirtyY2Y3OHaqUfQ6zU_8gD8SU',
+          apikey: '0HY9qKyvwty1hSzcTydn0AHAXPb0e2QzYQlMuQowS8U'
+        },
+        success: (response) => {
+          this.setData({response, loading: false });
+          
+        }
+      });
+      
   },
-  onTapCart() {
-    console.log('onTapCart');
-  },
-  onInput(e: { detail: { value: any; }; }) {
-    console.log('onInput', e.detail.value);
-  },
+  numberToCurrency(number: number, thousands: string) {
+    var includefee = false
+    if (number == undefined)
+        return 'undefined';
+    let fee = 0;
+    if (includefee) {
+        fee = 20000;
+    }
+    var parts = this.round(number + fee, 0).toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands);
+    return parts.join(thousands);
+}
+
+
 });
+
